f77334c18e048b2c0fabd06cc25ca79b04f9de54
Anthony Kelley <anthonymkelley@gmail.com>
2018-03-07 12:27:48 -0700

    add timer start and stop functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-  state={ time: new Date() }
+  state={ time: new Date(), secondsElapsed: 0 }
+
+  componentWillMount() {
+    setInterval( () => this.currentTime(),1000)
+  }
 
   currentTime() {
     this.setState({time: new Date()})
   }
 
-  componentWillMount() {
-    setInterval(()=>this.currentTime(),1000)
+  getSeconds = () => {
+    return ('0' + this.state.secondsElapsed % 60).slice(-2);
+  }
+
+  getMinutes = () => {
+    return Math.floor(this.state.secondsElapsed / 60)
+  }
+
+  handleStartClick = () => {
+    this.increment = setInterval( () => {
+      this.setState({
+        secondsElapsed: (this.state.secondsElapsed + 1)
+      })
+    }, 1000)
+  }
+
+  handleStopClick = () => {
+   clearInterval(this.increment)
   }
 
   render() {
     return (
       <div>
         <p className='clock'>{this.state.time.toLocaleTimeString()}</p>
+        <h4>{this.getMinutes()}:{this.getSeconds()}</h4>
+        <button onClick={this.handleStartClick}>Start</button>
+        <button onClick={this.handleStopClick}>Stop</button>
       </div>
     );
   }
